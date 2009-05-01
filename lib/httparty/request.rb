b2f0f12afcb3c4eae49fb565d73c6aefb180d3a6
Mark <rtec88@gmail.com>
2009-05-01 16:51:44 -0600

    added configurable timeout option to Net::HTTP with spec test

--- lib/httparty/request.rb
+++ lib/httparty/request.rb
@@ -41,12 +41,24 @@ module HTTParty
     end
 
     private
+      # def http
+      #   http = Net::HTTP.new(uri.host, uri.port, options[:http_proxyaddr], options[:http_proxyport])
+      #   http.use_ssl = (uri.port == 443)
+      #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      #   http
+      # end
+
       def http
         http = Net::HTTP.new(uri.host, uri.port, options[:http_proxyaddr], options[:http_proxyport])
         http.use_ssl = (uri.port == 443 || uri.scheme == 'https')
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        if options[:timeout] && options[:timeout].is_a?(Integer)
+          http.open_timeout = options[:timeout]
+          http.read_timeout = options[:timeout]
+        end
         http
       end
+      
 
       def configure_basic_auth
         @raw_request.basic_auth(options[:basic_auth][:username], options[:basic_auth][:password])
